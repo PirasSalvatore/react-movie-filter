e679f8e55da9910b9d2e9b0e387b3ce36a42e6ee
PirasSalvatore <torep.92@gmail.com>
2025-03-27 16:44:00 +0100

    ADD movies list Card

--- src/App.jsx
+++ src/App.jsx
@@ -1,13 +1,54 @@
 import { useState, useEffect } from "react"
 
+import movies from './assets/movies'
 
 
 function App() {
 
 
+
+
   return (
     <>
+      <div className="filter container">
+        <div className="col-6 m-3">
+          <label htmlFor="genreFilter" className="form-label ">Filter for genre</label>
+          <input
+            type="text"
+            className="form-control"
+            name="genreFilter"
+            id="genreFilter"
+            value={genreFilter}
+            placeholder="Select genre"
+            onChange={e => { setgenreFilter(e.target.value) }}
+          />
+        </div>
 
+      </div>
+
+      <div className="displayList container">
+
+        <h1 className="text-center p-3">Movies on shop</h1>
+
+        <div className="row mt-5 row-gap-2">
+          {
+            movies.map((movie, index) => {
+              return (
+                <div key={index} className="col-4 p-1">
+                  <div className="card">
+                    <div className="card-header">
+                      <h1>{movie.title}</h1>
+                    </div>
+                    <div className="card-body">
+                      <h3>{movie.genre}</h3>
+                    </div>
+                  </div>
+                </div>
+              )
+            })
+          }
+        </div>
+      </div>
     </>
   )
 }
